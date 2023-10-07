e105a2a6bf30f37cd71c45d719bf3646982fe981
David Berdik <berdikgit@theprogrammersworld.net>
2023-10-06 23:41:51 -0400

    Doing some refactoring before starting work on the migration to the new API.

---
+++ app/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    alias(libs.plugins.agp.app)
+    alias(libs.plugins.kotlin)
+}
+
+android {
+    namespace = "com.berdik.macsposed"
+    compileSdk = 34
+    buildToolsVersion = "34.0.0"
+
+    defaultConfig {
+        minSdk = 31
+        targetSdk = 34
+        versionCode = 6
+        versionName = "1.1.3"
+        applicationId = "com.berdik.macsposed"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles("proguard-rules.pro")
+            signingConfig = signingConfigs["debug"]
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        jvmToolchain(17)
+    }
+
+    packaging {
+        resources {
+            merges += "META-INF/xposed/*"
+            excludes += "**"
+        }
+    }
+
+    lint {
+        abortOnError = true
+        checkReleaseBuilds = false
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.constraintlayout)
+    implementation(libs.markdownview)
+    implementation(libs.libxposed.service)
+    compileOnly(libs.libxposed.api)
+}
